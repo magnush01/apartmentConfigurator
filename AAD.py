5b1f823a3be591a074298c4143ebf2a12a3e52a9
thegr8paul <116889227+thegr8paul@users.noreply.github.com>
2023-01-30 21:34:01 +0100

    Update AAD.py

--- AAD.py
+++ AAD.py
@@ -38,10 +38,17 @@ def main():
     scene.add(light)
 
     # Spotlight
-    spotLight = THREE.SpotLight.new(color, intensity)
-    spotLight.position.set(0, 1, 0)
-    spotLight.target.position.set(0, 0, 0)
-    scene.add(spotLight)
+    spotLight1 = THREE.SpotLight.new(color, intensity)
+    spotLight1.position.set(0, 15, 15)
+    spotLight1.target.position.set(0, 0, 0)
+    spotLight1.castShadow = True
+    scene.add(spotLight1)
+    
+    spotLight1 = THREE.SpotLight.new(color, intensity)
+    spotLight1.position.set(10, 10, 30)
+    spotLight1.castShadow = True
+    spotLight1.target.position.set(10, 10, 0)
+    scene.add(spotLight1)
 
     # Graphic Post Processing
     global composer
@@ -54,48 +61,77 @@ def main():
     # Set up Mouse orbit control
     controls = THREE.OrbitControls.new(camera, renderer.domElement)
     # Axis Helper
-    axesHelper = THREE.AxesHelper.new(100)
+    axesHelper = THREE.AxesHelper.new(10)
     scene.add(axesHelper)
     #-----------------------------------------------------------------------
     # Set up GUI
+    global floorsettings, apartment_type, apartment_variation, floor_length_x, floor_length_y, floor_ratio_1, floor_ratio_2, floor_ratio_3, floor_ratio_4
 
-    floor_length_x = 10
-    floor_length_y = 8
-    floor_ratio = 1/2
-    floor_iterations = 2
+    apartment_type = 1
+    apartment_variation = 1
+    floor_length_x = 15
+    floor_length_y = 15
+    floor_ratio_1 = 1/2
+    floor_ratio_2 = 1/2
+    floor_ratio_3 = 1/2
+    floor_ratio_4 = 1/2
+    floor_ratio_5 = 1/2
+    floor_ratio_6 = 1/2
+    floor_ratio_7 = 1/2
+    floor_ratio_8 = 1/2
 
     floorsettings = {
+        "apartment_type": apartment_type,
+        "apartment_variation": apartment_variation,
         "length_x": floor_length_x,
         "length_y": floor_length_y,
-        "public_private": floor_ratio,
-        "public_private": floor_ratio,
+        "ratio_1": floor_ratio_1,
+        "ratio_2": floor_ratio_2,
+        "ratio_3": floor_ratio_3,
+        "ratio_4": floor_ratio_4,
+        "ratio_5": floor_ratio_5,
+        "ratio_6": floor_ratio_6,
+        "ratio_7": floor_ratio_7,
+        "ratio_8": floor_ratio_8
     }
     floorsettings = Object.fromEntries(to_js(floorsettings))
 
     gui = window.dat.GUI.new()
+    param_folder = gui.addFolder('Apatrment_Generation')
+    param_folder.add(floorsettings,'apartment_type', 1,20)
+    param_folder.add(floorsettings,'apartment_variation', 1,20)
+
     param_folder = gui.addFolder('Floor Settings')
     param_folder.add(floorsettings,'length_x', 1,20,1)
     param_folder.add(floorsettings,'length_y', 1,20,1)
-    param_folder.add(floorsettings,'public_private', 0.2,0.7)
-    param_folder.add(floorsettings,'public_private', 0.2,0.7)
+
+    param_folder = gui.addFolder('Ratios')
+    param_folder.add(floorsettings,'ratio_1', 0.2,0.7)
+    param_folder.add(floorsettings,'ratio_2', 0.2,0.7)
+    param_folder.add(floorsettings,'ratio_3', 0.2,0.7)
+    param_folder.add(floorsettings,'ratio_4', 0.2,0.7)
+    param_folder.add(floorsettings,'ratio_5', 0.2,0.7)
+    param_folder.add(floorsettings,'ratio_6', 0.2,0.7)
+    param_folder.add(floorsettings,'ratio_7', 0.2,0.7)
+    param_folder.add(floorsettings,'ratio_8', 0.2,0.7)
     
-    param_folder.open()
+    '''param_folder.open()'''
 
     #-----------------------------------------------------------------------
     # Create Materials
     # Mesh Material
     global material, line_material
-    material = THREE.MeshBasicMaterial.new()
-    material.color = THREE.Color.new(0xA32A8C)
+    material = THREE.MeshPhongMaterial.new()
+    material.color = THREE.Color.new(0xFFFFFF)
     material.transparent = True
-    material.opacity = 0.6
+    material.opacity = 1
 
     # Line Material
     line_material = THREE.LineBasicMaterial.new()
     line_material.color = THREE.Color.new(0x1B1B1B)
     #-----------------------------------------------------------------------
     # Lists
-    global final_rooms, lines, new_rooms, offset_rooms, shapes
+    global final_rooms, lines, new_rooms, offset_rooms, shapes, room_sqaremeters
     #Roomlists
     # rooms = [room_1, room_2, ..., room_n] room_n = [point_1, point_2, point_3, point_4]
     final_rooms = []
@@ -103,6 +139,7 @@ def main():
     new_rooms = []
     offset_rooms = []
     shapes = []
+    room_sqaremeters = []
     
 
     # Impliment 
@@ -110,52 +147,46 @@ def main():
     firstroom = define_room(0, 0, floorsettings.length_x, 0, floorsettings.length_x , floorsettings.length_y, 0, floorsettings.length_y)
     new_rooms.append(firstroom)
 
+   
+
 
 
     #Apartements
-
-    Apartement(2,1,[1/2, 1/3, 3/4, 0, 0, 0, 2/3, 0, 0])
-
+    Apartment_GUI(floorsettings.apartment_type, floorsettings.apartment_variation,floorsettings.ratio_1,floorsettings.ratio_2,floorsettings.ratio_3,floorsettings.ratio_4,floorsettings.ratio_5,floorsettings.ratio_6,floorsettings.ratio_7,floorsettings.ratio_8)
+    '''Apartement(2,1,[1/2, 1/3, 3/4, 1/2, 0,0,0, 0, 0, 2/3, 0, 0])'''
+    '''Apartement(1,1,[1/2, 1/3, 2/4, 0, 0, 0, 2/3, 0, 0])'''
     #offset rooms for Wall vertices
     offset_out(new_rooms[0], 0.2)
     offset_in(final_rooms, 0.1)
     
-    drawrooms(offset_rooms)
-    extrude(firstroom, offset_rooms)
+    
+    drawrooms(final_rooms)
+    
     
 
+    drawrooms(offset_rooms)
+    drawrooms(offset_room_out)
+    
+    extrude(offset_room_out, offset_rooms)
+    #squaremeters functions
+    squaremeters(offset_rooms)
+    print(apartement_squaremeters(room_sqaremeters))
     render()
 #-----------------------------------------------------------------------
 # HELPER FUNCTIONS
 
 #GUI function
-def GUI():
-    global floorsettings, floor_length_x, floor_length_y, floor_iterations, floor_ratio
 
-    floor_length_x = 10
-    floor_length_y = 12
-    floor_ratio = 1/2
-    floor_iterations = 2
+# Apartment GUI function
+def Apartment_GUI(type, variation,r1,r2,r3,r4,r5,r6,r7,r8):
 
-    floorsettings = {
-        "length_x": floor_length_x,
-        "length_y": floor_length_y,
-        "public_private": floor_ratio,
-        "public_private": floor_ratio,
-    }
-    floorsettings = Object.fromEntries(to_js(floorsettings))
-
-    gui = window.dat.GUI.new()
-    param_folder = gui.addFolder('Floor Settings')
-    param_folder.add(floorsettings,'length_x', 1,20,1)
-    param_folder.add(floorsettings,'length_y', 1,20,1)
-    param_folder.add(floorsettings,'public_private', 0.2,0.7)
-    param_folder.add(floorsettings,'public_private', 0.2,0.7)
-    
-    param_folder.open()
+    if type == 1 & variation ==1:
+        Apartement(1,1,[r1, r2, r3, 0, 0, 0, r4, 0, 0])
+    pass
 
 
-# Apartement function
+
+# Apartment function
 def Apartement(apartement_type, number, ratios):
 
     if apartement_type == 1:
@@ -182,7 +213,7 @@ def Apartement_1(number,ratios):
 
 def Apartement_2(number,ratios):
     if number == 1:
-        variante(['V', 'H', 'V', 0, 0, 0, 'H', 0, 0],ratios)
+        variante(['V', 'H', 'H', 'H',0,0, 0, 0, 0],ratios)
     elif number == 2:
         variante(['V', 'H', 'V', 0, 0, 0, 'H', 0, 0],ratios)
     elif number == 3:
@@ -250,14 +281,13 @@ def drawrooms(rooms):
 def extrude(boundary, rooms):
     shape_geometry = THREE.Shape.new()
     #boundary extrusion
-    shape_geometry.moveTo(boundary[0][0],boundary[0][1])
-    shape_geometry.lineTo(boundary[1][0],boundary[1][1])
-    shape_geometry.lineTo(boundary[2][0],boundary[2][1])
-    shape_geometry.lineTo(boundary[3][0],boundary[3][1])
-    shape_geometry.lineTo(boundary[0][0],boundary[0][1])
+    shape_geometry.moveTo(boundary[0][0][0],boundary[0][0][1])
+    shape_geometry.lineTo(boundary[0][1][0],boundary[0][1][1])
+    shape_geometry.lineTo(boundary[0][2][0],boundary[0][2][1])
+    shape_geometry.lineTo(boundary[0][3][0],boundary[0][3][1])
+    shape_geometry.lineTo(boundary[0][0][0],boundary[0][0][1])
 
     #holes
-
     for room in rooms:
 
         room_hole = THREE.Path.new()
@@ -273,15 +303,21 @@ def extrude(boundary, rooms):
     
     extrudeSettings = (1,1,False,0,0,0,0)
 
-    '''extrudeSettings = {
-	steps: 200,
-	depth: 1600,
-	bevelEnabled: False,
-	bevelThickness: 1,
-	bevelSize: 1,
-	bevelOffset: 0,
-	bevelSegments: 1
-    }'''
+    extrudeSettings = {
+	"steps": 10,
+	"depth": 3,
+	"bevelEnabled": False,
+	"bevelThickness": 1,
+	"bevelSize": 1,
+	"bevelOffset": 0,
+	"bevelSegments": 1
+    }
+
+
+
+    extrudeSettings = Object.fromEntries(to_js(extrudeSettings))
+
+
     
 
     geometry = THREE.ExtrudeGeometry.new( shape_geometry, extrudeSettings)
@@ -298,9 +334,10 @@ def offset_in(rooms, d):
 
 #offset room outside function by changing the vertices
 def offset_out(room, d):
-    global new_rooms, offset_rooms
+    global new_rooms, offset_room_out
+    offset_room_out = []
     offset_room = define_room(room[0][0] - d , room[0][1] - d , room[1][0] + d , room[1][1] - d , room[2][0] + d , room [2][1] + d, room[3][0] - d, room[3][1] + d)
-    offset_rooms.append(offset_room)
+    offset_room_out.append(offset_room)
 
 
 
@@ -325,7 +362,6 @@ def subdivide_vertical(room, ratio):
     new_room2 = define_room(room[1][0] - ((room[1][0]-room[0][0]) * ratio), room[0][1], room[1][0], room[1][1], room[2][0], room[2][1], room[2][0] - ((room[2][0]-room[3][0]) * ratio), room[3][1])
     new_rooms.append(new_room1)
     new_rooms.append(new_room2)
-#next steps
 
 
 # Function to subdivide a room horizontally
@@ -336,19 +372,23 @@ def subdivide_horizontal(room, ratio):
     new_rooms.append(new_room1)
     new_rooms.append(new_room2)
 
-#-----------------------------------------------------------------------------------
+# Function to get list of room squaremeters
+def squaremeters(rooms):
 
-#NEXT STEPS
-# write symbol syntax to tell how to subdivide a room --> different layout types depending on the syntax
-# add different syntaxes
+    for room in rooms:
+        
+        x_size = (room[1][0] - room[0][0])
+        y_size = (room[3][1] - room[0][1])
+        squaremeters = x_size * y_size
+        room_sqaremeters.append(squaremeters)
 
-#make walls moveable and deleteable in the UI
-#gumball move
+    print(room_sqaremeters)
 
-
-#OPEN QUESTIONS
-#Number of Rooms follows boundary size?
-#How to add doors
+# Function for total Squaremeters
+def apartement_squaremeters(squaremeter_list):
+    
+    total_squaremeters = sum(squaremeter_list)
+    return total_squaremeters
 
 #-----------------------------------------------------------------------------------
 
